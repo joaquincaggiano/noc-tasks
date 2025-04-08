ffce522c759fa3de11fd071628d206f53b9e1992
Joaquin Serra caggiano <joaquinserracaggiano@192.168.1.5>
2025-04-08 20:54:57 -0300

    inyeccion de la instancia repositorio en el check service

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,11 @@
 import { CronService } from "../cron/cron-service";
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
+
+const fileSystemlogRepository = new LogRepositoryImpl(
+  new FileSystemDatasource()
+);
 
 export class Server {
   static async start() {
@@ -7,8 +13,9 @@ export class Server {
 
     const job = CronService.createJob("*/5 * * * * *", () => {
       const url = "https://www.google.com";
-      
+
       new CheckService(
+        fileSystemlogRepository,
         () => console.log(`Service ${url} is ok`),
         (error) => console.log(error)
       ).execute(url);
