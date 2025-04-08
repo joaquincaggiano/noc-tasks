939cff7cfa01fb8b39373f917c52dce8d5aa0f7d
Joaquin Serra caggiano <joaquinserracaggiano@192.168.1.5>
2025-04-08 14:52:34 -0300

    instalando cron

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import { Server } from "./presentation/server";
+import { CronJob } from 'cron';
 
 const main = () => {
   Server.start();
+
+  const job = new CronJob(
+	'* * * * * *',
+	() => {
+      console.log("You will see this message every second");
+    }
+  );
 };
 
 main();
