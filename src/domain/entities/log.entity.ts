246aa5ffb1dfd9c08d2a5d5aa236c7cbeabba420
Joaquin Serra caggiano <joaquinserracaggiano@192.168.1.10>
2025-04-09 16:30:51 -0300

    nueva propiedad origin

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -4,19 +4,33 @@ export enum LogSeverityLevel {
   high = "high",
 }
 
+export interface LogEntityOptions {
+  level: LogSeverityLevel;
+  message: string;
+  origin: string;
+  createdAt?: Date;
+}
+
 export class LogEntity {
   public level: LogSeverityLevel;
   public message: string;
   public createdAt: Date;
+  public origin: string;
 
-  constructor(message: string, level: LogSeverityLevel) {
+  constructor({
+    message,
+    level,
+    origin,
+    createdAt = new Date(),
+  }: LogEntityOptions) {
     this.message = message;
     this.level = level;
-    this.createdAt = new Date();
+    this.createdAt = createdAt;
+    this.origin = origin;
   }
 
   static fromJson = (json: string): LogEntity => {
-    const { level, message, createdAt } = JSON.parse(json);
+    const { level, message, createdAt, origin } = JSON.parse(json);
 
     if (!message) throw new Error("Message is required");
     if (!level) throw new Error("Level is required");
@@ -28,8 +42,7 @@ export class LogEntity {
       throw new Error("Level is invalid");
     }
 
-    const log = new LogEntity(message, level);
-    log.createdAt = new Date(createdAt);
+    const log = new LogEntity({ message, level, origin, createdAt });
 
     return log;
   };
