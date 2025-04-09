0f6f53000e52ad5e9f90938851fd60530b6a0ad9
Joaquin Serra caggiano <joaquinserracaggiano@192.168.1.5>
2025-04-08 21:25:06 -0300

    config env

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,7 @@
 import { Server } from "./presentation/server";
-import { CronJob } from 'cron';
 
 const main = () => {
   Server.start();
-
-  const job = new CronJob(
-	'* * * * * *',
-	() => {
-      console.log("You will see this message every second");
-    }
-  );
 };
 
 main();
