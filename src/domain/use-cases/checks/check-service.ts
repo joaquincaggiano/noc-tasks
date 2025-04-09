f11c0f1dd19081f74b2fa41671bfa028245fec01
Joaquin Serra caggiano <joaquinserracaggiano@192.168.1.5>
2025-04-08 21:06:51 -0300

    funciones succeess y error can be undefined

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -5,8 +5,8 @@ interface CheckServiceUseCase {
   execute(url: string): Promise<boolean>;
 }
 
-type SuccessCallback = () => void;
-type ErrorCallback = (error: string) => void;
+type SuccessCallback = (() => void) | undefined;
+type ErrorCallback = ((error: string) => void) | undefined;
 
 export class CheckService implements CheckServiceUseCase {
   constructor(
@@ -28,7 +28,7 @@ export class CheckService implements CheckServiceUseCase {
         LogSeverityLevel.low
       );
       this.logRepository.saveLog(log);
-      this.successCallback();
+      this.successCallback && this.successCallback();
       return true;
     } catch (error) {
       const errorMessage = `${url} is not working. ${error}`;
@@ -36,7 +36,7 @@ export class CheckService implements CheckServiceUseCase {
       const log = new LogEntity(errorMessage, LogSeverityLevel.high);
 
       this.logRepository.saveLog(log);
-      this.errorCallback(errorMessage);
+      this.errorCallback && this.errorCallback(errorMessage);
 
       return false;
     }
