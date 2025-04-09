b5f1f660eb0664ac598c1d95aaccb08a35fbf941
Joaquin Serra caggiano <joaquinserracaggiano@192.168.1.10>
2025-04-09 17:31:51 -0300

    nodemailer

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,8 @@
-import { CronService } from "../cron/cron-service";
+import { CronService } from "./cron/cron-service";
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
+import { EmailService } from "./email/email.service";
 
 const fileSystemlogRepository = new LogRepositoryImpl(
   new FileSystemDatasource()
@@ -11,14 +12,25 @@ export class Server {
   static async start() {
     console.log("Server is running...");
 
-    const job = CronService.createJob("*/5 * * * * *", () => {
-      const url = "https://www.google.com";
+    const emailService = new EmailService();
 
-      new CheckService(
-        fileSystemlogRepository,
-        () => console.log(`Service ${url} is ok`),
-        (error) => console.log(error)
-      ).execute(url);
+    emailService.sendEmail({
+      to: "jcaggiano@desaway.es",
+      subject: "Logs de sistema",
+      htmlBody: `
+        <h1>Logs de sistema - NOC</h1>
+        <p>Hola, este es un mensaje de prueba</p>
+      `,
     });
+
+    // CronService.createJob("*/5 * * * * *", () => {
+    //   const url = "https://www.google.com";
+
+    //   new CheckService(
+    //     fileSystemlogRepository,
+    //     () => console.log(`Service ${url} is ok`),
+    //     (error) => console.log(error)
+    //   ).execute(url);
+    // });
   }
 }
