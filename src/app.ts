e06677c55bb68b67fd566e5e41622647d9668922
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-04-15 20:21:23 +0200

    mongo database class

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import { Server } from "./presentation/server";
+import { envs } from "./config/plugins/envs.plugin";
+import { MongoDatabase } from "./data/mongodb/init";
 
-const main = () => {
+const main = async () => {
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME,
+  });
+
   Server.start();
 };
 
