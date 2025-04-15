6912654874e4eeda9bd29ec3dd4dca301ebc3fb4
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-04-15 21:35:53 +0200

    utilizacion de mongo log datasource

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -4,9 +4,15 @@ import { FileSystemDatasource } from "../infrastructure/datasources/file-system.
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { EmailService } from "./email/email.service";
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
+import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
+import { LogSeverityLevel } from "../domain/entities/log.entity";
 
 const fileSystemlogRepository = new LogRepositoryImpl(
   new FileSystemDatasource()
+);
+
+const mongoLogRepository = new LogRepositoryImpl(
+  new MongoLogDatasource()
 );
 
 const emailService = new EmailService();
@@ -14,6 +20,11 @@ const emailService = new EmailService();
 export class Server {
   static async start() {
     console.log("Server is running...");
+
+    const logs = await fileSystemlogRepository.getLogs(
+      LogSeverityLevel.low
+    );
+    console.log(logs);
 
     // const sendEmailLogs = new SendEmailLogs(
     //   emailService,
@@ -26,7 +37,7 @@ export class Server {
     //   const url = "https://www.google.com";
 
     //   new CheckService(
-    //     fileSystemlogRepository,
+    //     mongoLogRepository,
     //     () => console.log(`Service ${url} is ok`),
     //     (error) => console.log(error)
     //   ).execute(url);
