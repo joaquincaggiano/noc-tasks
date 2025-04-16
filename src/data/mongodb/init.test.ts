bb28dc2a8aadd2779ef03ac6b6893831d38dd871
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-04-17 01:03:49 +0200

    testeando la conexion a mongo

---
+++ src/data/mongodb/init.test.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { MongoDatabase } from "./init";
+
+describe("init MongoDB", () => {
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
+  test("should connect to MongoDB", async () => {
+    const connected = await MongoDatabase.connect({
+      dbName: process.env.MONGO_DB_NAME!,
+      mongoUrl: process.env.MONGO_URL!,
+    });
+
+    expect(connected).toBe(true);
+  });
+
+  test("should throw an error", async () => {
+    try {
+      const connected = await MongoDatabase.connect({
+        dbName: process.env.MONGO_DB_NAME!,
+        mongoUrl: "mongodb://joaquincaggiano:123456@localhostttttt:27017/",
+      });
+      expect(true).toBe(false);
+    } catch (error) {}
+  });
+});
--- src/data/mongodb/init.ts
+++ src/data/mongodb/init.ts
@@ -13,9 +13,9 @@ export class MongoDatabase {
       await mongoose.connect(mongoUrl, {
         dbName,
       });
-      console.log("Connected to MongoDB");
+
+      return true;
     } catch (error) {
-      console.log("Error connecting to MongoDB");
       throw error;
     }
   }
